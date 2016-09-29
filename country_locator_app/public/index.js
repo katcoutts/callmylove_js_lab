cddacc513486295c8fdf9323d2bf0fa5b093bb8a
Katrina Coutts <katcoutts@hotmail.co.uk>
2016-09-29 09:19:51 +0100

    some refactoring and distance calculation added

--- country_locator_app/public/index.js
+++ country_locator_app/public/index.js
@@ -1,12 +1,13 @@
+// https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en
+
 var countries;
-var countryName;
 var map;
+
 
 var requestComplete = function(){
   if(this.status !== 200) return;
   var jsonString = this.responseText;
   countries = JSON.parse(jsonString);
-  console.log(countries);
   var country = function(countryName){
   for (var country of countries){
     if (country.name === countryName){
@@ -14,28 +15,62 @@ var requestComplete = function(){
     }
   }
   }
-  console.log(country(countryName));
   var callingCode = country(countryName).callingCodes;
-  console.log(callingCode);
   var timeZone = country(countryName).timezones;
-  console.log(timeZone);
-  var callingCodeText = document.getElementById('calling-code');
-  callingCodeText.innerText = callingCode;
-  var timeZoneText = document.getElementById('time-zone');
-  timeZoneText.innerText = timeZone;
-
+  printCallingCode(callingCode);
+  printTimeZone(timeZone);
   var container = document.getElementById('map');
-  var laT = country(countryName).latlng[0];
-  var lnG = country(countryName).latlng[1];
+  laT = country(countryName).latlng[0];
+  lnG = country(countryName).latlng[1];
   var center = { lat: laT, lng: lnG };
   var zoom = 3;
   map = new Map (container, center, zoom);
-  map.addMarker(center);
-  navigator.geolocation.getCurrentPosition(function(position){
-    map.addMarker({lat: position.coords.latitude, lng: position.coords.longitude});
-  })
+  map.addMarker(center); 
+  setYourLocation();  
 }
 
+var setYourLocation = function(){
+  navigator.geolocation.getCurrentPosition(function(position){
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    map.addMarker({lat: position.coords.latitude, lng: position.coords.longitude});
+    getDistances(laT, lnG, latitude, longitude);
+  });
+}
+
+var getDistances = function(lat1, long1, lat2, long2){
+    var distance = getDistance({lat: lat1, lng: long1}, {lat: lat2, lng: long2});
+    var km = distance/1000;
+    var miles = Math.round((km / 1.6) * 100) / 100;
+    var distanceBox = document.getElementById('distance');
+    distanceBox.innerText = "You are " + miles + " miles apart!";  
+}
+
+var rad = function(x) {
+  return x * Math.PI / 180;
+};
+
+var getDistance = function(p1, p2) {
+  var R = 6378137; // Earth’s mean radius in meter
+  var dLat = rad(p2.lat - p1.lat);
+  var dLong = rad(p2.lng - p1.lng);
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(rad(p1.lat)) * Math.cos(rad(p2.lat)) *
+    Math.sin(dLong / 2) * Math.sin(dLong / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d; // returns the distance in meter
+};
+
+var printCallingCode = function(callingCode){
+  var callingCodeText = document.getElementById('calling-code');
+  callingCodeText.innerText = callingCode;
+}
+
+var printTimeZone = function(timeZone){
+  var timeZoneText = document.getElementById('time-zone');
+  timeZoneText.innerText = timeZone;
+}
 
 var makeRequest = function(url, callback){
   var request = new XMLHttpRequest();
@@ -47,12 +82,10 @@ var makeRequest = function(url, callback){
 
 var app = function(){
   var url = "http://localhost:5000";
-  countryName = "Russia";
   var button = document.querySelector('button');
   button.onclick = function(){
     var input = document.getElementById('inputText').value;
     countryName = input;
-    console.log(countryName);
     makeRequest(url, requestComplete)
   } 
 
@@ -63,33 +96,3 @@ window.onload = app;
 
 
 
-// var map;
-
-// var handleArthurButtonClick = function(){
-//  var coords = {  lat: 55.945101, lng: -3.162346};
-//  map.addListener(coords);
-// }
-
-// var handleMyCurrentLocationClick = function(){
-//  navigator.geolocation.getCurrentPosition(function(position){
-//    map.addListener({lat: position.coords.latitude, lng:position.coords.longitude})
-//  });
-// }
-
-
-// var initialize = function(){
-//  var container = document.getElementById('map');
-//  var center = { lat: 55.945101, lng: -3.162346}
-//  var zoom = 15;
-//  map = new Map (container, center, zoom);
-
-//  var arthurButton = document.getElementById("take-me-to-the-top-of-edinburgh");
-//  arthurButton.onclick = handleArthurButtonClick;
-
-//  var currentLocationButton = document.getElementById("where-am-i");
-//  currentLocationButton.onclick = handleMyCurrentLocationClick;
-
-//  map.addMarker(center);
-//  map.addClickEvent();
- 
-// }
--- country_locator_app/public/map.js
+++ country_locator_app/public/map.js
@@ -5,43 +5,14 @@ var Map = function(container, coords, zoom){
    zoom: zoom
  });
 
- var infoWindow = new google.maps.InfoWindow({
-   content: "Imagine a great description!"
- });
-
  this.addMarker = function(coords){
    var marker = new google.maps.Marker({
      position: coords,
      map: this.googleMap,
      animation: google.maps.Animation.BOUNCE,
      icon: "heart.png"
-   });
-
-   infoWindow.setPosition(coords);
-   infoWindow.setContent("This has a latitude of:" +coords["lat"]+ " and a longitude of"+coords["lng"]+"");
-
-   marker.addListener('click', function(){
-     infoWindow.open(this.googleMap,marker);
-   })
-   
+   });   
  }
-
- this.addClickEvent = function(){
-   google.maps.event.addListener(this.googleMap, 'click', function(event){
-
-     var position = { lat: event.latLng.lat() , lng: event.latLng.lng() }
-     this.addMarker(position)
-   }.bind(this))
-
- };
-
- this.addListener = function(coords){
-  this.googleMap.panTo(coords);
-  this.googleMap.setCenter(coords);
-  this.addMarker(coords);
-
-};
-
 
 
 
